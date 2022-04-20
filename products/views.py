f9819691365c263132342775104e7ccbd53d093b
cleason <cocuche53@gmail.com>
2022-04-20 12:44:47 +0200

    Commitfirst

--- products/views.py
+++ products/views.py
@@ -1,10 +1,20 @@
+from contextvars import Context
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request, *args, **kwargs):
-    return render(request, 'index.html')
+    name = "Cleason"
+    number = 33
+    my_list = [3,35,6,7,9]
+    context = {
+        'nom':name,
+        'numero':number,
+        'maList':my_list
+    }
+    return render(request, 'index.html', context)
 
 def contact(request):
     return HttpResponse("<h1> Contact me </h1>")
